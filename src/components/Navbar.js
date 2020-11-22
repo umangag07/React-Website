d0bee7fe72393d8c7a783d7cfd9372e15142c650
umang <25umangagarwal@gmail.com>
2020-11-22 23:47:23 +0530

    Added react-router-dom dependency title&logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                TRAVELO <img src={process.env.PUBLIC_URL + '/images/Logo.png'} alt="logo"></img>
+                 {/*  <i className='fab fa-typo3' />  This is the logo from font awesome cdn*/}
+                </Link> 
+            </div>
+        </nav>
+        </>
     )
 }
 
